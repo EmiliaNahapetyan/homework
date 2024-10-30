e58480efc7818909f8eb2368260c6b679f36238f
EmiliaNahapetyan <emilianahapetyan.tt261-2@polytechnic.am>
2024-10-30 19:44:39 +0400

    new

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,10 +1,15 @@
 def bubble_sort(arr):
     n=len(arr)
     for i in range(n):
+        swapped=False
         for j  in range(n-i-1):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
+                swapped=True
+        if not swapped:
+            print("Array is already sorted!")
+            break
     return arr
 
-arr=[9, 3, 99, 5, 7, 4, 22, 10]
+arr=[3, 4, 5, 7, 9, 10, 22, 99]
 print(bubble_sort(arr))
